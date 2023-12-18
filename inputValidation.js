b790559580ec24cdd3fe737f1892f95e3215f96a
kabenari <swarup25decnov@gmail.com>
2023-12-18 18:59:17 +0530

    ok

--- inputValidation.js
+++ inputValidation.js
@@ -1,14 +1,42 @@
 const express = require('express')
-
+const zod= require('zod')
 const app = express();
+
+const schema= zod.array(zod.number());
+
+//
+//{
+//   email: string => email
+//   password : atleast 8 letters
+//   country : "IN" , "US"
+//}
+
+
+const schema2 = zod.object({
+    email: zod.string().email(),
+    password : zod.string().min(8),
+    country: zod.literal("IN").or(zod.literal("US")),
+    kidneys : zod.array(zod.number())
+})
 
 app.use(express.json());
 
 app.post("/health-checkup",function(req,res){
     const kidneys = req.body.kidneys;
-    const kidneyLength = kidneys.length;
+    const response = schema.safeParse(kidneys)
 
-    res.send("your Kidney Length is " + kidneyLength);
+    if(!response.success){
+        res.status(411).json({
+            msg:"invalid input"
+        })
+        return
+    }
+
+    res.send({
+        response
+    })
+
 })
+
 
 app.listen(3000)
